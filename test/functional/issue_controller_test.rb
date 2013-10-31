7bce685b9237b71dd4c374d709d29615ccf43616
mcberros <mcberros@gmail.com>
2013-10-31 22:55:49 +0100

    Fixed test

--- test/functional/issue_controller_test.rb
+++ test/functional/issue_controller_test.rb
@@ -5,7 +5,7 @@ class IssueControllerTest < ActionController::TestCase
 
 
   test "should send an email when post a new issue" do
-    IssueMailer.any_instance.expects(:new_issue)
+    IssueMailer.any_instance.expects(:send_confirmation_link)
     post :create, {text: 'a text', summary: 'a summary', images: ['image1']}
   end
 end
